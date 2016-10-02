c24f16e320d0d65c826be9e9c48f8206b1960fd7
eric <lostaznfound@gmail.com>
2016-10-02 15:22:45 -0500

    removed the transpilers and istanbul test purpose javascript insertions

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,23 @@
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
+// import babel from 'rollup-plugin-babel';
+// import babelrc from 'babelrc-rollup';
+// import istanbul from 'rollup-plugin-istanbul';
+
+// import uglify from 'rollup-plugin-uglify';
+// import { minify } from 'uglify-js'; // use uglify's harmony branch to minify es2016 code
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
-export default {
+import { rollup } from 'rollup'
+
+module.exports = {
   entry: 'lib/index.js',
   plugins: [
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
+    // babel(babelrc()),
+    // istanbul({
+    //   exclude: ['test/**/*', 'node_modules/**/*']
+    // })    
+    // uglify({}, minify)
   ],
   external: [...external],
   targets: [
